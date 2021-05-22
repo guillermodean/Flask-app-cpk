89d8279f2345ea5d8e4164eafecce1aa8a0bd55a
Guillermoç <guillermodean@hotmail.com>
2021-05-22 13:05:13 +0200

    conexion mysql db

--- app.py
+++ app.py
@@ -2,8 +2,21 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask.wrappers import Request
+from flaskext import mysql
+from flask import request
+import os
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='Guillermo11'
+app.config['MYSQL_DATABASE_DB']='cpk'
+
+#mysql= mysql.MySQL(app)
+mysql = mysql.MySQL(app, prefix="mysql1", host='localhost', user='root',password='Guillermo11',db='cpk', autocommit=True)
+mysql.init_app(app)
 
 from products import products
 
@@ -18,6 +31,22 @@ def graficas():
 @app.route('/templates/tablas')
 def tablas():
     return render_template('tabla.html')
+
+@app.route('/añadir_cpk')
+def formulario():
+    return render_template('Formulario.html')
+
+@app.route('/añadir_cpk',methods =['POST'])
+def addcpk():
+    if request.method=='POST':
+        button = request.form['Button']
+        seat = request.form['seat']
+        cpk = request.form ['cpk']
+        cursor = mysql.get_db().cursor()
+        cursor.execute('INSERT INTO `cpk-resultst`(`Button`, `seat`, `cpk`) VALUES (%s,%s,%s)',(button,seat,cpk))
+
+    return 'received'
+
 
 #PARA LANZAR LOS USUARIOS Y LAS CONEXIONES
 
